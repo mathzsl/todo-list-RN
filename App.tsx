0d4949f3c8af81ed171932c4a1fadbe978d88a5f
mathzsl <maths.soares99@gmail.com>
2023-09-01 23:41:11 -0300

    chore: initial project setup

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" translucent />
+      <Home />
+    </>
+  );
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,4 @@
+import { Text, View } from "react-native";
+export function Home() {
+  return <View></View>;
+}
